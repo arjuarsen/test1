f00408a8a58200215ba9c7c7e7e009106cf00657
arjun <37340972+arjuarsen@users.noreply.github.com>
2019-10-17 17:09:16 +0530

    Create myapp.py

---
+++ myapp.py
@@ -0,0 +1,8 @@
+import ConfigParser
+
+ configParser = ConfigParser.RawConfigParser()   
+ configFilePath = r'config.txt'
+ configParser.read(configFilePath)
+ config()
+ self.env= configParser.get('your-config','env')
+ 
